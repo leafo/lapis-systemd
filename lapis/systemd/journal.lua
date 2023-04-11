773cdb204132a500cbd8966e29746c16b2485233
leaf corcoran <leafot@gmail.com>
2023-04-11 11:23:32 -0700

    add a journal reader utility

--- lapis/systemd/journal.lua
+++ lapis/systemd/journal.lua
@@ -1,8 +1,33 @@
 local ffi = require("ffi")
 local socket = require("socket")
-local config = require("lapis.config")
+local lapis_config = require("lapis.config")
 ffi.cdef([[  int sd_journal_print(int priority, const char *format, ...);
   int sd_journal_send(const char *format, ...);
+
+
+  typedef struct sd_journal sd_journal;
+
+  int sd_journal_open(sd_journal **ret, int flags);
+  int sd_journal_close(sd_journal *j);
+
+  int sd_journal_next(sd_journal *j);
+  int sd_journal_previous(sd_journal *j);
+  int sd_journal_seek_head(sd_journal *j);
+  int sd_journal_seek_tail(sd_journal *j);
+
+  int sd_journal_add_match(sd_journal *j, const void *data, size_t size);
+
+  int sd_journal_get_data(sd_journal *j, const char *field, const void **data, size_t *length);
+  int sd_journal_enumerate_data(sd_journal *j, const void **data, size_t *length);
+
+  int sd_journal_wait(sd_journal *j, int64_t timeout_usec);
+
+  enum {
+    SD_JOURNAL_LOCAL_ONLY = 1,
+    SD_JOURNAL_RUNTIME_ONLY = 2,
+    SD_JOURNAL_SYSTEM = 4,
+    SD_JOURNAL_CURRENT_USER = 8
+  };
 ]])
 local lib = ffi.load("systemd")
 local site_name
@@ -16,6 +41,7 @@ site_name = function()
 end
 local log
 log = function(msg, opts)
+  local config = lapis_config.get()
   if not (config.systemd and config.systemd.journal) then
     return 
   end
@@ -28,6 +54,98 @@ log = function(msg, opts)
   }
   return lib.sd_journal_send(unpack(commands))
 end
+local JournalReader
+do
+  local _class_0
+  local _base_0 = {
+    make_listener = function(self)
+      local journal_ptr = ffi.new("sd_journal*[1]")
+      local name = site_name()
+      local flags = lib.SD_JOURNAL_LOCAL_ONLY
+      local ret = lib.sd_journal_open(journal_ptr, flags or 0)
+      if ret < 0 then
+        error("Error opening journal: " .. tostring(tonumber(ret)))
+      end
+      local journal = journal_ptr[0]
+      ffi.gc(journal, lib.sd_journal_close)
+      do
+        name = site_name()
+        if name then
+          local match = "SITE=" .. name
+          lib.sd_journal_add_match(journal, match, #match)
+        end
+      end
+      if self.config.filter_unit then
+        local match = "_SYSTEMD_UNIT=" .. unit_name
+        lib.sd_journal_add_match(journal, match, #match)
+      end
+      lib.sd_journal_seek_tail(journal)
+      lib.sd_journal_previous(journal)
+      return coroutine.wrap(function()
+        while true do
+          local _continue_0 = false
+          repeat
+            local r = lib.sd_journal_next(journal)
+            if r < 0 then
+              error("Error reading journal: " .. tostring(tonumber(r)))
+            end
+            if r == 0 then
+              lib.sd_journal_wait(journal, -1)
+              _continue_0 = true
+              break
+            end
+            local entry = self:read_entry(journal)
+            coroutine.yield(entry)
+            _continue_0 = true
+          until true
+          if not _continue_0 then
+            break
+          end
+        end
+      end)
+    end,
+    read_entry = function(self, journal)
+      local entry = { }
+      local data_ptr = ffi.new("const void*[1]")
+      local length = ffi.new("size_t[1]")
+      local field = lib.sd_journal_enumerate_data(journal, data_ptr, length)
+      while field > 0 do
+        local data = ffi.string(data_ptr[0], length[0])
+        local key, value = data:match("^(.-)=(.*)$")
+        if key and value then
+          entry[key] = value
+        end
+        field = lib.sd_journal_enumerate_data(journal, data_ptr, length)
+      end
+      return entry
+    end
+  }
+  _base_0.__index = _base_0
+  _class_0 = setmetatable({
+    __init = function(self, config)
+      if config == nil then
+        config = { }
+      end
+      self.config = config
+    end,
+    __base = _base_0,
+    __name = "JournalReader"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  JournalReader = _class_0
+end
+local listen
+listen = function(opts)
+  return JournalReader(opts):make_listener()
+end
 return {
-  log = log
+  log = log,
+  listen = listen
 }
--- lapis/systemd/journal.moon
+++ lapis/systemd/journal.moon
@@ -5,11 +5,36 @@ ffi = require "ffi"
 
 socket = require "socket"
 
-config = require "lapis.config"
+lapis_config = require "lapis.config"
 
 ffi.cdef [[
   int sd_journal_print(int priority, const char *format, ...);
   int sd_journal_send(const char *format, ...);
+
+
+  typedef struct sd_journal sd_journal;
+
+  int sd_journal_open(sd_journal **ret, int flags);
+  int sd_journal_close(sd_journal *j);
+
+  int sd_journal_next(sd_journal *j);
+  int sd_journal_previous(sd_journal *j);
+  int sd_journal_seek_head(sd_journal *j);
+  int sd_journal_seek_tail(sd_journal *j);
+
+  int sd_journal_add_match(sd_journal *j, const void *data, size_t size);
+
+  int sd_journal_get_data(sd_journal *j, const char *field, const void **data, size_t *length);
+  int sd_journal_enumerate_data(sd_journal *j, const void **data, size_t *length);
+
+  int sd_journal_wait(sd_journal *j, int64_t timeout_usec);
+
+  enum {
+    SD_JOURNAL_LOCAL_ONLY = 1,
+    SD_JOURNAL_RUNTIME_ONLY = 2,
+    SD_JOURNAL_SYSTEM = 4,
+    SD_JOURNAL_CURRENT_USER = 8
+  };
 ]]
 
 lib = ffi.load "systemd"
@@ -21,6 +46,7 @@ site_name = ->
   name
 
 log = (msg, opts) ->
+  config = lapis_config.get!
   return unless config.systemd and config.systemd.journal
 
   priority = opts and opts.priority or 6
@@ -28,4 +54,71 @@ log = (msg, opts) ->
   commands = { "MESSAGE=#{msg}", "PRIORITY=#{priority}", "SITE=#{name}" }
   lib.sd_journal_send unpack commands
 
-{ :log }
+class JournalReader
+  new: (@config={}) =>
+
+  make_listener: =>
+    journal_ptr = ffi.new("sd_journal*[1]")
+
+    name = site_name!
+
+    flags = lib.SD_JOURNAL_LOCAL_ONLY
+
+    ret = lib.sd_journal_open(journal_ptr, flags or 0)
+    if ret < 0
+      error "Error opening journal: #{tonumber ret}"
+
+    journal = journal_ptr[0]
+
+    ffi.gc journal, lib.sd_journal_close
+
+    if name = site_name!
+      match = "SITE=" .. name
+      lib.sd_journal_add_match journal, match, #match
+
+    if @config.filter_unit
+      match = "_SYSTEMD_UNIT=" .. unit_name
+      lib.sd_journal_add_match journal, match, #match
+
+    -- seek to the end of the journal
+    lib.sd_journal_seek_tail journal
+    lib.sd_journal_previous journal
+
+    coroutine.wrap ->
+      while true
+        r = lib.sd_journal_next(journal)
+        if r < 0
+          error "Error reading journal: #{tonumber r}"
+
+        if r == 0
+          -- sleep until next entry is available
+          lib.sd_journal_wait(journal, -1) -- -1 for an indefinite wait
+          continue
+
+        entry = @read_entry journal
+        coroutine.yield entry
+
+  read_entry: (journal) =>
+    entry = {}
+
+    data_ptr = ffi.new "const void*[1]"
+    length = ffi.new "size_t[1]"
+
+    field = lib.sd_journal_enumerate_data(journal, data_ptr, length)
+    while field > 0
+      data = ffi.string data_ptr[0], length[0]
+      key, value = data\match("^(.-)=(.*)$")
+
+      if key and value
+        entry[key] = value
+
+      field = lib.sd_journal_enumerate_data(journal, data_ptr, length)
+
+    entry
+
+-- create a infinite iterator that will listen for incoming messages from the
+-- journal
+listen = (opts) ->
+  JournalReader(opts)\make_listener!
+
+{ :log, :listen }
